322db5b5f43b2f92077fe3fc24d3549f1acf3ea7
kjpranata <kevjepe@gmail.com>
2021-09-29 16:32:52 +0700

    Need to check on changes for timer

--- tests/client/notification_service_tests.cpp
+++ tests/client/notification_service_tests.cpp
@@ -9,21 +9,34 @@
 #include <string>
 
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 #include <chrono>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
 namespace xpx_chain_sdk::tests {
 
+void print(const boost::system::error_code&, boost::asio::deadline_timer* timer, int* count, bool* isReceived, Address recipient){
+    if (*count < 15){
+        ++(*count);
+        timer->expires_from_now(boost::posix_time::seconds(1));
+        timer->async_wait(boost::bind(print, boost::asio::placeholders::error, timer, count));
+        client->notifications()->removeConfirmedAddedNotifiers(recipient);
+        EXPECT_TRUE(isReceived);
+    }
+}
+
 #define TEST_CLASS NotificationService
 
     boost::asio::io_context io;
-    boost::asio::deadline_timer timer(io, boost::posix_time::seconds(60));
+    boost::posix_time::seconds interval(60);
+    // boost::asio::deadline_timer timer(io, boost::posix_time::seconds(60));
+    boost::asio::deadline_timer timer(io, interval);
 
     ClientData clientData;
     auto client = xpx_chain_sdk::getClient(std::make_shared<xpx_chain_sdk::Config>(getTestConfiguration()));
     auto account = getTestAccount(clientData.privateKey);
 
-    TEST(TEST_CLASS, addConfirmedAddedNotifier) {
+        TEST(TEST_CLASS, addConfirmedAddedNotifier) {
 
         //create transaction
         Address recipient(clientData.publicKeyContainer);
@@ -38,6 +51,7 @@ namespace xpx_chain_sdk::tests {
         bool isReceived = false;
         std::mutex receivedMutex;
         std::unique_lock<std::mutex> lock(receivedMutex);
+        int count = 0;
 
         //Handler
         ConfirmedAddedNotifier notifier = [&transferTransaction, &isReceived](const TransactionNotification& notification) {
@@ -54,87 +68,127 @@ namespace xpx_chain_sdk::tests {
         client->notifications()->addConfirmedAddedNotifiers(recipient, {notifier});
         EXPECT_TRUE(client->transactions()->announceNewTransaction(transferTransaction->binary()));
        
-        timer.async_wait([&isReceived, &recipient] ( ... ) {
-            client->notifications()->removeConfirmedAddedNotifiers(recipient);
-            EXPECT_TRUE(isReceived);
-        });
+        timer.async_wait(boost::bind(print, boost::asio::placeholders::error, &timer, &count, &isReceived, &recipient));
+
+        // timer.async_wait([&isReceived, &recipient] ( ... ) {
+        //     client->notifications()->removeConfirmedAddedNotifiers(recipient);
+        //     EXPECT_TRUE(isReceived);
+        // });
     
         io.run();
     }
 
-    TEST(TEST_CLASS, addUnconfirmedAddedNotifiers){
-        //create transaction
-        Address recipient(clientData.publicKeyContainer);
-        Mosaic mosaic(-4613020131619586570, 100);
-        MosaicContainer mosaics{ mosaic };
-        RawBuffer message("test message");
+//     TEST(TEST_CLASS, addUnconfirmedAddedNotifiers){
+//         //create transaction
+//         Address recipient(clientData.publicKeyContainer);
+//         Mosaic mosaic(-4613020131619586570, 100);
+//         MosaicContainer mosaics{ mosaic };
+//         RawBuffer message("test message");
 
-        //sign transaction
-        std::unique_ptr<TransferTransaction> transferTransaction = CreateTransferTransaction(recipient, mosaics, message);
-        account->signTransaction(transferTransaction.get());
+//         //sign transaction
+//         std::unique_ptr<TransferTransaction> transferTransaction = CreateTransferTransaction(recipient, mosaics, message);
+//         account->signTransaction(transferTransaction.get());
 
-        bool isReceived = false;
-        std::mutex receivedMutex;
-        std::unique_lock<std::mutex> lock(receivedMutex);
+//         bool isReceived = false;
+//         std::mutex receivedMutex;
+//         std::unique_lock<std::mutex> lock(receivedMutex);
         
-        //Handler
-        UnconfirmedAddedNotifier notifier = [&transferTransaction, &isReceived](const TransactionNotification& notification){
-            if (notification.meta.hash == ToHex(transferTransaction->hash())) {
-                EXPECT_EQ(ToHex(transferTransaction->hash()), notification.meta.hash);
+//         //Handler
+//         UnconfirmedAddedNotifier notifier = [&transferTransaction, &isReceived](const TransactionNotification& notification){
+//             if (notification.meta.hash == ToHex(transferTransaction->hash())) {
+//                 EXPECT_EQ(ToHex(transferTransaction->hash()), notification.meta.hash);
 
-                auto transactionInfo = client->transactions()->getTransactionInfo(TransactionGroup::Unconfirmed, notification.meta.hash);
-                EXPECT_EQ(TransactionType::Transfer,transactionInfo.get()->type);
+//                 auto transactionInfo = client->transactions()->getTransactionInfo(TransactionGroup::Unconfirmed, notification.meta.hash);
+//                 EXPECT_EQ(TransactionType::Transfer,transactionInfo.get()->type);
 
-                isReceived = true;
-            }
-        };    
+//                 isReceived = true;
+//             }
+//         };    
 
-        client->notifications()->addUnconfirmedAddedNotifiers (recipient, {notifier});
-        EXPECT_TRUE(client->transactions()->announceNewTransaction(transferTransaction->binary()));
+//         client->notifications()->addUnconfirmedAddedNotifiers (recipient, {notifier});
+//         EXPECT_TRUE(client->transactions()->announceNewTransaction(transferTransaction->binary()));
 
-        timer.async_wait([&isReceived, &recipient] ( ... ) {
-            client->notifications()->removeUnconfirmedAddedNotifiers(recipient);
-            EXPECT_TRUE(isReceived);
-        });
+//         timer.async_wait([&isReceived, &recipient] ( ... ) {
+//             client->notifications()->removeUnconfirmedAddedNotifiers(recipient);
+//             EXPECT_TRUE(isReceived);
+//         });
 
-        io.run();
-    }
+//         io.run();
+//     }
 
-   TEST(TEST_CLASS, addUnconfirmedRemovedNotifiers){
-        //create transaction
-        Address recipient(clientData.publicKeyContainer);
-        Mosaic mosaic(-4613020131619586570, 100);
-        MosaicContainer mosaics{ mosaic };
-        RawBuffer message("test message");
+//    TEST(TEST_CLASS, addUnconfirmedRemovedNotifiers){
+//         //create transaction
+//         Address recipient(clientData.publicKeyContainer);
+//         Mosaic mosaic(-4613020131619586570, 100);
+//         MosaicContainer mosaics{ mosaic };
+//         RawBuffer message("test message");
 
-        //sign transaction
-        std::unique_ptr<TransferTransaction> transferTransaction = CreateTransferTransaction(recipient, mosaics, message);
-        account->signTransaction(transferTransaction.get());
+//         //sign transaction
+//         std::unique_ptr<TransferTransaction> transferTransaction = CreateTransferTransaction(recipient, mosaics, message);
+//         account->signTransaction(transferTransaction.get());
 
-        bool isReceived = false;
-        std::mutex receivedMutex;
-        std::unique_lock<std::mutex> lock(receivedMutex);
+//         bool isReceived = false;
+//         std::mutex receivedMutex;
+//         std::unique_lock<std::mutex> lock(receivedMutex);
         
-        //Handler
-        UnconfirmedRemovedNotifier notifier = [&transferTransaction, &isReceived](const UnconfirmedRemovedTransactionNotification& transaction){
-            if (transaction.meta.hash == ToHex(transferTransaction->hash())){
-                EXPECT_EQ(ToHex(transferTransaction->hash()), transaction.meta.hash);
+//         //Handler
+//         UnconfirmedRemovedNotifier notifier = [&transferTransaction, &isReceived](const UnconfirmedRemovedTransactionNotification& transaction){
+//             if (transaction.meta.hash == ToHex(transferTransaction->hash())){
+//                 EXPECT_EQ(ToHex(transferTransaction->hash()), transaction.meta.hash);
 
-                auto transactionInfo = client->transactions()->getAnyTransactionInfo(transaction.meta.hash);
-                EXPECT_EQ(TransactionType::Transfer,transactionInfo.get()->type);
+//                 auto transactionInfo = client->transactions()->getAnyTransactionInfo(transaction.meta.hash);
+//                 EXPECT_EQ(TransactionType::Transfer,transactionInfo.get()->type);
 
-                isReceived = true;
-            }
-        };
+//                 isReceived = true;
+//             }
+//         };
                
-        client->notifications()->addUnconfirmedRemovedNotifiers(recipient, {notifier});
-        EXPECT_TRUE(client->transactions()->announceNewTransaction(transferTransaction->binary()));
+//         client->notifications()->addUnconfirmedRemovedNotifiers(recipient, {notifier});
+//         EXPECT_TRUE(client->transactions()->announceNewTransaction(transferTransaction->binary()));
 
-        timer.async_wait([&isReceived, &recipient] ( ... ) {
-            client->notifications()->removeUnconfirmedRemovedNotifiers(recipient);
-            EXPECT_TRUE(isReceived);
-        });      
+//         timer.async_wait([&isReceived, &recipient] ( ... ) {
+//             client->notifications()->removeUnconfirmedRemovedNotifiers(recipient);
+//             EXPECT_TRUE(isReceived);
+//         });      
 
-        io.run();
-    }   
+//         io.run();
+//     } 
+
+//    TEST(TEST_CLASS, addStatusNotifiers){
+//         //create transaction
+//         Address recipient(clientData.publicKeyContainer);
+//         Mosaic mosaic(-4613020131619586570, 100);
+//         MosaicContainer mosaics{ mosaic };
+//         RawBuffer message("test message");
+
+//         //sign transaction
+//         std::unique_ptr<TransferTransaction> transferTransaction = CreateTransferTransaction(recipient, mosaics, message);
+//         account->signTransaction(transferTransaction.get());
+
+//         bool isReceived = false;
+//         std::mutex receivedMutex;
+//         std::unique_lock<std::mutex> lock(receivedMutex);
+        
+//         //Handler
+//         StatusNotifier notifier = [&transferTransaction, &isReceived](const TransactionStatusNotification& status){
+//             if (status.hash == ToHex(transferTransaction->hash())){
+//                 EXPECT_EQ(ToHex(transferTransaction->hash()), status.hash);
+
+//                 auto transactionInfo = client->transactions()->getAnyTransactionInfo(status.hash);
+//                 EXPECT_EQ(TransactionType::Transfer,transactionInfo.get()->type);
+
+//                 isReceived = true;
+//             }
+//         };
+               
+//         client->notifications()->addStatusNotifiers(recipient, {notifier});
+//         EXPECT_TRUE(client->transactions()->announceNewTransaction(transferTransaction->binary()));
+
+//         timer.async_wait([&isReceived, &recipient] ( ... ) {
+//             client->notifications()->removeStatusNotifiers(recipient);
+//             // EXPECT_TRUE(isReceived);
+//         });      
+
+//         io.run();
+//     } 
 }
