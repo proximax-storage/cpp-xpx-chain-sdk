3f3baff0e65a518fc54abfd8657c45319d03203f
jess-xinyi <86717706+jess-xinyi@users.noreply.github.com>
2021-10-18 15:41:32 +0800

    R21BC-190, R21BC-191, R21BC-192

--- tests/client/network_service_tests.cpp
+++ tests/client/network_service_tests.cpp
@@ -26,6 +26,42 @@ namespace xpx_chain_sdk::tests {
         EXPECT_FALSE(networkConfig.data.supportedEntityVersions.empty());
         EXPECT_FALSE(networkConfig.data.config.empty());
     }
+    TEST(TEST_CLASS, getNetworkConfigAtHeight_zero) {
+        EXPECT_THROW ({
+            try {
+                NetworkConfig networkConfig = client->network()->getNetworkConfigAtHeight(0);
+            } 
+            catch (xpx_chain_sdk::InvalidRequest& e) {
+                EXPECT_STREQ("Server rejected the request. Error code: 404", e.what() );
+                throw;
+            } 
+        }, xpx_chain_sdk::InvalidRequest);
+    }
+
+    TEST(TEST_CLASS, getNetworkConfigAtHeight_negative) {
+        EXPECT_THROW ({
+            try {
+                NetworkConfig networkConfig = client->network()->getNetworkConfigAtHeight(-1);
+            } 
+            catch (xpx_chain_sdk::InvalidRequest& e) {
+                EXPECT_STREQ("Server rejected the request. Error code: 409", e.what() );
+                throw;
+            } 
+        }, xpx_chain_sdk::InvalidRequest);
+    }
+
+    TEST(TEST_CLASS, getNetworkConfigAtHeight_max) {
+        EXPECT_THROW ({
+            try {
+                uint64_t blockchainHeight = UINT64_MAX;
+                NetworkConfig networkConfig = client->network()->getNetworkConfigAtHeight(blockchainHeight);
+            } 
+            catch (xpx_chain_sdk::InvalidRequest& e) {
+                EXPECT_STREQ("Server rejected the request. Error code: 409", e.what() );
+                throw;
+            } 
+        }, xpx_chain_sdk::InvalidRequest);
+    }
 
     TEST(TEST_CLASS, getNetworkInfo) {
         NetworkInfo networkInfo = client->network()->getNetworkInfo();
@@ -43,5 +79,42 @@ namespace xpx_chain_sdk::tests {
         NetworkVersion networkVersion = client->network()->getNetworkVersionAtHeight(1);
         EXPECT_TRUE(networkVersion.data.height > 0);
         EXPECT_TRUE(networkVersion.data.version > 0);
+    }
+
+    TEST(TEST_CLASS, getNetworkVersionAtHeight_zero) {
+        EXPECT_THROW ({
+            try {
+                NetworkVersion networkVersion = client->network()->getNetworkVersionAtHeight(0);
+            } 
+            catch (xpx_chain_sdk::InvalidRequest& e) {
+                EXPECT_STREQ("Server rejected the request. Error code: 404", e.what() );
+                throw;
+            } 
+        }, xpx_chain_sdk::InvalidRequest);
+    }
+
+    TEST(TEST_CLASS, getNetworkVersionAtHeight_negative) {
+        EXPECT_THROW ({
+            try {
+                NetworkVersion networkVersion = client->network()->getNetworkVersionAtHeight(-1);
+            } 
+            catch (xpx_chain_sdk::InvalidRequest& e) {
+                EXPECT_STREQ("Server rejected the request. Error code: 409", e.what() );
+                throw;
+            } 
+        }, xpx_chain_sdk::InvalidRequest);
+    }
+
+    TEST(TEST_CLASS, getNetworkVersionAtHeight_max) {
+        EXPECT_THROW ({
+            try {
+                uint64_t blockchainHeight = UINT64_MAX;
+                NetworkVersion networkVersion = client->network()->getNetworkVersionAtHeight(blockchainHeight);
+            } 
+            catch (xpx_chain_sdk::InvalidRequest& e) {
+                EXPECT_STREQ("Server rejected the request. Error code: 409", e.what() );
+                throw;
+            } 
+        }, xpx_chain_sdk::InvalidRequest);
     }
 }
