f166330c0347a613fe7af6f09c1c1ed7f3dbc4af
Viktor.Markvardt <viktor.markvardt@gmail.com>
2022-01-11 15:03:48 +0200

    Fixed tabulation

--- include/xpxchaincpp/model/transaction/transaction_type.h
+++ include/xpxchaincpp/model/transaction/transaction_type.h
@@ -10,36 +10,36 @@
 namespace xpx_chain_sdk {
 	
 	/// Transaction type.
-	enum class TransactionType: uint16_t {
-		Unknown = 0x0000,
-		Transfer = 0x4154,
-		Mosaic_Definition = 0x414D,
-		Mosaic_Supply_Change = 0x424D,
-		Mosaic_Levy_Change = 0x434D, // not defined yet
-		Register_Namespace = 0x414E,
-		Address_Alias = 0x424E,
-		Mosaic_Alias = 0x434E,
-		Modify_Multisig_Account = 0x4155,
-		Aggregate_Complete = 0x4141,
-		Aggregate_Bonded = 0x4241,
-		Lock_Funds = 0x4148,
-		Address_Property = 0x4150,
-		Mosaic_Property = 0x4250,
-		Transaction_Property = 0x4350,
-		Secret_Lock = 0x4152,
-		Secret_Proof = 0x4252,
-        Storage_Payment = 0x4a62,
-		Account_Link = 0x414C,
-		Prepare_Bc_Drive = 0x4162,
-		Data_Modification = 0x4262,
-		Download = 0x4362,
-		Download_Payment = 0x4962,
-        Drive_Closure = 0x4e62,
-		Data_Modification_Approval = 0x4462,
-		Data_Modification_Cancel = 0x4562,
-		Finish_Download = 0x4862,
-		Replicator_Onboarding = 0x4662,
-	};
+    enum class TransactionType: uint16_t {
+        Unknown = 0x0000,
+        Transfer = 0x4154,
+        Mosaic_Definition = 0x414D,
+        Mosaic_Supply_Change = 0x424D,
+        Mosaic_Levy_Change = 0x434D, // not defined yet
+        Register_Namespace = 0x414E,
+        Address_Alias = 0x424E,
+        Mosaic_Alias = 0x434E,
+        Modify_Multisig_Account = 0x4155,
+        Aggregate_Complete = 0x4141,
+        Aggregate_Bonded = 0x4241,
+        Lock_Funds = 0x4148,
+        Address_Property = 0x4150,
+        Mosaic_Property = 0x4250,
+        Transaction_Property = 0x4350,
+        Secret_Lock = 0x4152,
+        Secret_Proof = 0x4252,
+		Storage_Payment = 0x4a62,
+        Account_Link = 0x414C,
+        Prepare_Bc_Drive = 0x4162,
+        Data_Modification = 0x4262,
+        Download = 0x4362,
+        Download_Payment = 0x4962,
+		Drive_Closure = 0x4e62,
+        Data_Modification_Approval = 0x4462,
+        Data_Modification_Cancel = 0x4562,
+        Finish_Download = 0x4862,
+        Replicator_Onboarding = 0x4662,
+    };
 	
 	/// Returns transaction name by \a type.
 	const char* GetTransactionName(TransactionType type);
