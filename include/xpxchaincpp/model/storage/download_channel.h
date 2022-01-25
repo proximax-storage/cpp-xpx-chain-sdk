dc960df1e87c70a6909a0395320905917be19e8b
Viktor.Markvardt <viktor.markvardt@gmail.com>
2022-01-25 18:20:44 +0200

    Implemented entities: replicator, download channel, pages

---
+++ include/xpxchaincpp/model/storage/download_channel.h
@@ -0,0 +1,38 @@
+/**
+*** Copyright 2021 ProximaX Limited. All rights reserved.
+*** Use of this source code is governed by the Apache 2.0
+*** license that can be found in the LICENSE file.
+**/
+#pragma once
+
+#include <cstdint>
+
+namespace xpx_chain_sdk {
+
+    class CumulativePayment {
+    public:
+        std::string replicator;
+        uint64_t payment;
+    };
+
+    class DownloadChannelData {
+    public:
+        std::string id;
+        std::string consumer;
+        std::string drive;
+        uint64_t downloadSize;
+        uint16_t downloadApprovalCount;
+        std::vector<std::string> listOfPublicKeys;
+        std::vector<CumulativePayment> cumulativePayments;
+    };
+
+	class DownloadChannel {
+	public:
+        DownloadChannelData data;
+	};
+
+	class MultipleDownloadChannels {
+	public:
+		std::vector<DownloadChannel> channels;
+	};
+}
