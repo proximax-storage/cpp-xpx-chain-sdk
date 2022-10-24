f9693d98a19f47fd488812f84aaec66ebe6632b5
Viktor.Markvardt <viktor.markvardt@gmail.com>
2022-10-24 20:37:41 +0300

    Disabled tests by default

--- CMakeGlobalFunctions.cmake
+++ CMakeGlobalFunctions.cmake
@@ -1,13 +1,15 @@
 # Method to get GTest and link it from https://google.github.io/googletest/quickstart-cmake.html
 
-include(FetchContent)
-FetchContent_Declare(
-  googletest
-  URL https://github.com/google/googletest/archive/609281088cfefc76f9d0ce82e1ff6c30cc3591e5.zip
-)
-# For Windows: Prevent overriding the parent project's compiler/linker settings
-set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-FetchContent_MakeAvailable(googletest)
+if (XPX_CHAIN_SDK_BUILD_TESTS)
+    include(FetchContent)
+    FetchContent_Declare(
+            googletest
+            URL https://github.com/google/googletest/archive/609281088cfefc76f9d0ce82e1ff6c30cc3591e5.zip
+    )
+    # For Windows: Prevent overriding the parent project's compiler/linker settings
+    set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+    FetchContent_MakeAvailable(googletest)
+endif()
 
 function(add_single_test TARGET_NAME SOURCE_CPP)
     enable_testing()
